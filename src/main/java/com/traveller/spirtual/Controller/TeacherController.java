78f3426147e3045bd3c2fc768c8973dc3946fe10
DESKTOP-K6IHCUD\Sahan_Kumarage <kavishkasahandj@gmail.com>
2023-12-14 15:46:32 +0530

    teacher's api created

--- src/main/java/com/traveller/spirtual/Controller/TeacherController.java
+++ src/main/java/com/traveller/spirtual/Controller/TeacherController.java
@@ -1,9 +1,11 @@
 package com.traveller.spirtual.Controller;
 
 import com.traveller.spirtual.Service.TeacherService;
+import com.traveller.spirtual.dao.TeacherEntity;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -11,4 +13,14 @@ public class TeacherController {
 
     @Autowired
     TeacherService service;
+
+    @GetMapping("/getTeacher")
+    public List<TeacherEntity> getTeacher(){
+        return service.getTeachers();
+    }
+
+    @PostMapping("/enterTeacher")
+    public void enterTeacher(@RequestBody TeacherEntity teacher){
+        service.enterTeacher(teacher);
+    }
 }
--- src/main/java/com/traveller/spirtual/Service/TeacherService.java
+++ src/main/java/com/traveller/spirtual/Service/TeacherService.java
@@ -6,14 +6,28 @@ import com.traveller.spirtual.dto.Teacher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TeacherService {
 
     @Autowired
     TeacherRepository repository;
 
-    public void enterTeacher(TeacherEntity teacher){
+    public void enterTeacher(TeacherEntity teacher) {
+        TeacherEntity teach = new TeacherEntity();
+        teach.setTeacherName(teacher.getTeacherName());
+        teach.setTeacherSubject(teacher.getTeacherSubject());
+        teach.setTeachSchool(teacher.getTeachSchool());
+        teach.setProImg(teacher.getProImg());
+        teach.setAddress(teacher.getAddress());
+        teach.setContact(teacher.getContact());
 
+        repository.save(teach);
     }
 
+
+    public List<TeacherEntity> getTeachers() {
+        return (List<TeacherEntity>) repository.findAll();
+    }
 }
